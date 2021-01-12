e7283907cb27a3ef98197f8f2c00f8e9732c29ba
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    ginkgo: Silence clang warnings during kernel build
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
     * remove deprecated LATEST_CLANG flag while im at it
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 12.0
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # NFC
 TARGET_USES_NQ_NFC := true
